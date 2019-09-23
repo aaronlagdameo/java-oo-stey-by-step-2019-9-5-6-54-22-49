bfd4e80127f68e2592de830f77bcbf7b8e889947
Aaron Lagdameo <aaron.kristoffer.lagdameo@oocl.com>
2019-09-23 10:36:17 +0800

    Finished Practice-9

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,34 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach " + this.klass.getDisplayName() + ".";
+    }
+
+    String introduceWith(Student student) {
+        if (klass.getNumber() == student.getKlass().getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
