e0d83cc73fc222436518e53b93264197aabfcd04
Aaron Lagdameo <aaron.kristoffer.lagdameo@oocl.com>
2019-09-23 10:18:33 +0800

    Finished Practice-8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,28 @@
 package practice08;
 
 public class Klass {
+
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return this.number;
+    }
+
+    String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
 }
+
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,26 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduction = super.introduce() + " I am a Student.";
+        if (this.klass.getLeader() == null) {
+            introduction += " I am at " + this.klass.getDisplayName() + ".";
+        } else if (this.getName().equals(this.klass.getLeader().getName())) {
+            introduction += " I am Leader of " + this.klass.getDisplayName() + ".";
+        }
+        return introduction;
+    }
 }
